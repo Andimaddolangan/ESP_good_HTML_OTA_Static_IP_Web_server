ce488921814af9ed50aef76cd752e094b13e8636
nicelight <harrizmat@mail.ru>
2017-04-18 15:10:20 +0700

    new html april 17

--- HTML_pages.h
+++ HTML_pages.h
@@ -1,4 +1,4 @@
-const static char html_page_mech[] PROGMEM = R"=====(
+const static char html_main_page[] PROGMEM = R"=====(
  <!DOCTYPE html>
 <html>
 <head>
@@ -6,43 +6,34 @@ const static char html_page_mech[] PROGMEM = R"=====(
 	<meta charset='utf-8'>
 </head>
 <body>
-<center><h1>Mechamics </h1></center>
+<center><h1>Комфортный дом </h1></center>
 <br>
-<input type='button' value='Kartina Otkr' style='width:300Px;height:200Px' onclick=\"location.href='mech?button=51';\">
-<input type='button' value='Kartina Stop' style='width:300Px;height:200Px' onclick=\"location.href='mech?button=52';\">
-<input type='button' value='Kartina Zakr' style='width:300Px;height:200Px' onclick=\"location.href='mech?button=53';\">
 <br><br><br>
-<input type='button' value='Reshetki Otkr' style='width:300Px;height:200Px' onclick=\"location.href='mech?button=54';\">
-<input type='button' value='Reshetki Zakr' style='width:300Px;height:200Px' onclick=\"location.href='mech?button=55';\">
+<input type='button' value='Лампы включить' style='width:300Px;height:200Px' onclick="location.href='leds?lamp=3';">
+<input type='button' value='Лампы потушить' style='width:300Px;height:200Px' onclick="location.href='leds?lamp=0';">
+
 <br><br><br>
-<input type='button' value='Gilotina Otkr' style='width:300Px;height:200Px' onclick=\"location.href='mech?button=56';\">
-<input type='button' value='Gilotina Zakr' style='width:300Px;height:200Px' onclick=\"location.href='mech?button=57';\">";
+<input type='button' value='1 Лампа' style='width:300Px;height:100Px' onclick="location.href='leds?lamp=1';">
+<input type='button' value='2 Лампы' style='width:300Px;height:100Px' onclick="location.href='leds?lamp=2';">
+
 <br><br><br>
-<input type='button' value='Balkon dver Otkr' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=58';\">
-<input type='button' value='Balkon dver Zakr' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=59';\">
-<br><br><br> 
-<input type='button' value='Pugalka steni Otkr' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=60';\">
-<input type='button' value='Pugalka steni Zakr' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=61';\">
+<input type='button' value='Ультрафиолет Включить' style='width:400Px;height:150Px' onclick="location.href='leds?uv=1';">
+<input type='button' value='Ультрафиолет потушить' style='width:200Px;height:150Px' onclick="location.href='leds?uv=0';">
+
 <br><br><br>
-<input type='button' value='Dim zal na 3 sek' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=62';\">
-<input type='button' value='Dim Rakohod na 3 sek' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=63';\">
+<input type='button' value='Красный Вкл' style='width:200Px;height:200Px' onclick="location.href='leds?red=1';">
+<input type='button' value='Зеленый Вкл' style='width:200Px;height:200Px' onclick="location.href='leds?green=1';">
+<input type='button' value='Синий Вкл' style='width:200Px;height:200Px' onclick="location.href='leds?blue=1';">
 <br><br><br>
-<input type='button' value='Kost polka Otkr' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=64';\">
-<input type='button' value='Kost polka Zakr' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=65';\">
-<br><br><br>
-<input type='button' value='Monitor Otkl' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=66';\">
-<input type='button' value='Monitor Vkl' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=67';\">
-<br><br><br> 
-<input type='button' value='Dver vihod Otkr' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=68';\">
-<input type='button' value='Dver vihod  Zakr' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=69';\">
-<br><br><br> 
-<input type='button' value='Dim zal Vkl pitanie' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=70';\">
-<input type='button' value='Dim zal Otkl pitanie' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=71';\">
-<br><br><br>
-<input type='button' value='Dim rakoh Vkl pitanie' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=72';\">
-<input type='button' value='Dim rakoh Otkl pitanie' style='width:450Px;height:200Px' onclick=\"location.href='mech?button=73';\">
+<input type='button' value='Красный Потушить' style='width:200Px;height:200Px' onclick="location.href='leds?red=0';">
+<input type='button' value='Зеленый Потушить' style='width:200Px;height:200Px' onclick="location.href='leds?green=0';">
+<input type='button' value='Синий Потушить' style='width:200Px;height:200Px' onclick="location.href='leds?blue=0';">
+
+<br><br><br><br><br><br>
+<input type='button' value='ОЙ ВСЕ' style='width:600Px;height:300Px' onclick="location.href='leds?lamp=-1';">
+
+
 <br><br><br><br><br><br><br><br><br>
- You can control: <br><br><br> <a href='/game'> Game process </a> <br><br><br>__________ <a href='/mech'> Mechanics </a> <br><br><br>_____________________ <a href='/light'> Light </a> <br><br><br><br>
 </body>
 </html>
 )=====";
